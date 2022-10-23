a8e9c526892b024fa5a3f0e911387970e86d59be
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2022-10-23 18:28:07 +0200

    Small release script improvements

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,14 +2,21 @@
 set -euo pipefail
 
 DOCS_VERSION=$1
-GITOPS_ORIGIN="https://bizzkit-platform@dev.azure.com/bizzkit-platform/Bizzkit/_git/gitops-partnerportal"
+DEFAULT_ALIAS="latest"
+
+GITOPS_REMOTE="https://bizzkit-platform@dev.azure.com/bizzkit-platform/Bizzkit/_git/gitops-partnerportal"
+GITOPS_REMOTE_NAME="gitops"
+GITOPS_BRANCH="main"
+GITOPS_DOCS_DIRECTORY="docs"
 
 # Add origin to integrate with mike
-git remote add gitops $GITOPS_ORIGIN
+git remote add $GITOPS_REMOTE_NAME $GITOPS_ORIGIN
 
 # Generate version patches
-mike deploy --branch main --remote gitops --prefix docs --update-aliases $DOCS_VERSION latest
-mike set-default --branch main --remote gitops --prefix docs --push latest
+mike deploy --branch $GITOPS_BRANCH --remote $GITOPS_REMOTE_NAME --prefix $GITOPS_DOCS_DIRECTORY --update-aliases $DOCS_VERSION $DEFAULT_ALIAS
+mike set-default --branch $GITOPS_BRANCH --remote $GITOPS_REMOTE_NAME --prefix $GITOPS_DOCS_DIRECTORY --push $DEFAULT_ALIAS
 
 # Clean up after mike
-git remote remove gitops
+rm -r site
+git branch -D $GITOPS_BRANCH
+git remote remove $GITOPS_REMOTE_NAME
